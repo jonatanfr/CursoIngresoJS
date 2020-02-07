45faf2a3491ba4631de1d22f2ce1a2958874aa50
alumno <alumno@utnfra.com.ar>
2020-02-06 22:09:23 -0300

    clase 2 de programacion parte 3

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -3,6 +3,32 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
-{
+{   
+    var numeroUno;
+
+    var numeroDos;
+
+    var dividendo;
+    
+    var divisor;
+
+    var resto;
+
+    //ingresar datos id
+
+    numeroUno= document.getElementById("numeroDividendo").value;
+    numeroDos= document.getElementById("numeroDivisor").value;
+
+    //parselar
+    dividendo=parseInt(numeroUno);
+    divisor=parseInt(numeroDos);
+
+    //dividir
+    resto=dividendo%divisor;
+
+    //alert 
+    
+    alert ("El Resto es "+resto);
+
 	
 }
