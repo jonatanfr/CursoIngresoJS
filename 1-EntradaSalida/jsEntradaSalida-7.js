c6112dcd37fbd16734a4f32a041c171d259db967
alumno <alumno@utnfra.com.ar>
2020-02-06 21:55:00 -0300

    Clase 2 de programacion parte 2

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,131 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
-}
+	//declaramos las variables
+
+        var numeroUnoIngresado;
+    
+        var numeroDosingresado;
+    
+        var numeroUnoParseado;
+    
+        var numeroDosParseado;
+    
+        var resultado;
+    
+        //Obtener los valores por id
+    
+        numeroUnoIngresado = document.getElementById("numeroUno").value;
+        numeroDosingresado = document.getElementById("numeroDos").value;
+    
+        //Parseo de datos
+        
+        numeroUnoParseado= parseInt(numeroUnoIngresado);
+        numeroDosParseado= parseInt(numeroDosingresado);
+    
+        //Operacion
+        resultado = numeroUnoParseado + numeroDosParseado;
+       
+    
+        //Mostrar resultado
+        alert("La suma es "+resultado);
+    }
+
 
 function restar()
 {
-	
+	//declaramos las variables
+
+        var numeroUnoIngresado;
+    
+        var numeroDosingresado;
+    
+        var numeroUnoParseado;
+    
+        var numeroDosParseado;
+    
+        var resultado;
+    
+        //Obtener los valores por id
+    
+        numeroUnoIngresado = document.getElementById("numeroUno").value;
+        numeroDosingresado = document.getElementById("numeroDos").value;
+    
+        //Parseo de datos
+        
+        numeroUnoParseado= parseInt(numeroUnoIngresado);
+        numeroDosParseado= parseInt(numeroDosingresado);
+    
+        //Operacion
+        resultado = numeroUnoParseado - numeroDosParseado;
+       
+    
+        //Mostrar resultado
+        alert("La resta es "+resultado);	
 }
 
 function multiplicar()
 { 
-	
+	//declaramos las variables
+
+    var numeroUnoIngresado;
+    
+        var numeroDosingresado;
+    
+        var numeroUnoParseado;
+    
+        var numeroDosParseado;
+    
+        var resultado;
+    
+        //Obtener los valores por id
+    
+        numeroUnoIngresado = document.getElementById("numeroUno").value;
+        numeroDosingresado = document.getElementById("numeroDos").value;
+    
+        //Parseo de datos
+        
+        numeroUnoParseado= parseInt(numeroUnoIngresado);
+        numeroDosParseado= parseInt(numeroDosingresado);
+    
+        //Operacion
+        resultado = numeroUnoParseado * numeroDosParseado;
+       
+    
+        //Mostrar resultado
+        alert("La Multiplicacion es "+resultado);
 }
 
 function dividir()
 {
-	
+	//declaramos las variables
+
+    var numeroUnoIngresado;
+    
+        var numeroDosingresado;
+    
+        var numeroUnoParseado;
+    
+        var numeroDosParseado;
+    
+        var resultado;
+    
+        //Obtener los valores por id
+    
+        numeroUnoIngresado = document.getElementById("numeroUno").value;
+        numeroDosingresado = document.getElementById("numeroDos").value;
+    
+        //Parseo de datos
+        
+        numeroUnoParseado= parseInt(numeroUnoIngresado);
+        numeroDosParseado= parseInt(numeroDosingresado);
+    
+        //Operacion
+        resultado = numeroUnoParseado / numeroDosParseado;
+        
+    
+        //Mostrar resultado
+        alert("La division es "+resultado);
+
 }
 
