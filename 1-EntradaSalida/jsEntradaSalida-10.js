6deeaaf6ca2f75cbc962b4d57ea08ca7829615f1
jony <jonatanfr86@gmail.com>
2020-02-09 03:29:00 -0300

    final de semana

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,32 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    var descuento;
+
+    var porcentaje
+
+    var resultado;
+
+    porcentaje = 25
+
+
+    var importe
+
+    importe= document.getElementById("importe").value;
+    
+    importe = parseInt(importe);
+
+    descuento = importe*porcentaje/100;
+    
+    resultado = descuento;
+
+    descuento = porcentaje * importe/100;
+    resultado = importe - descuento;
+
+    document.getElementById("resultado").value = resultado;
+    
+
+
+
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,33 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
+        var resultado;
+
+        var aumento;
+
+        var porsentaje;
+
+        var importe;
+
+
+        porcentaje = 10/100;
+
+
+        importe = document.getElementById("sueldo").value;
+        importe= parseInt(importe);
+
+        aumento= importe * porcentaje;
+        resultado= importe + aumento;
+       
+        document.getElementById("resultado").value = resultado;
+
+
+
+
+
+
+
+
+
 	
 }
