ded392a735bc87e61a1b102a0132e569ac924614
jony <jonatanfr86@gmail.com>
2020-02-16 00:58:32 -0300

    sabado

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -4,6 +4,31 @@ function mostrar()
 var laHora = document.getElementById('hora').value;
 
 //alert (laHora);
+
+switch(laHora)
+{
+                        case "7":
+                        alert("Es de manana");
+                        break;
+                        case "8":
+                            alert("Es de manana");
+                            break; 
+                        case "9":
+                            alert("Es de manana");
+                        break;
+                        case "10":
+                            alert("Es de manana");
+                        break;
+                        case "11":
+                        alert("Es de manana");
+                        break;
+
+
+
+
+
+
+}                          
 	
 	
 
