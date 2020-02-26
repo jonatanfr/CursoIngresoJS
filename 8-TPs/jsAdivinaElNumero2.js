da813215f887aa63fe667fb0017edb4aeee9f3bf
jony <jonatanfr86@gmail.com>
2020-02-26 00:24:58 -0300

    tps feriados

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -10,12 +10,18 @@ Desde  6 Intentos hasta 10:”falta técnica”
 Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
-var contadorIntentos;
+var contadorIntentos = 0;
+var numeroUsuario;
+
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
 	 
+	numeroSecreto = Math.floor(Math.random () * (101 - 1) + 1);
+		console.log(numeroSecreto);
+		alert("comenzamos!");
+	contadorIntentos = 0;
 	
 
 }
@@ -23,6 +29,60 @@ function comenzar()
 function verificar()
 {
 	
-	
+	while (numeroUsuario != numeroSecreto)
+	{	
+		numeroUsuario = document.getElementById("numero").value;
+		contadorIntentos ++
+		numeroUsuario = parseInt(numeroUsuario);
+		break;
 
+	}	
+		if(numeroUsuario == numeroSecreto)
+		{	
+			alert("Usted es un ganador!!! y en solo "+contadorIntentos+"intentos");
+
+			switch (contadorIntentos)
+			{
+				case 1:
+						alert("Usted es un psiquico");
+						break;
+				case 2:
+						alert("Exclente percepcion");
+						break;
+				case 3:
+						alert("Esto es suerte");
+						break;
+				case 4:
+						alert("Excelente tecnica");
+						break;
+				case 5:
+						alert("Usted es la media");
+						break;
+				case 6:
+				case 7:
+				case 8:
+				case 9:
+				case 10:
+						alert("Falta tecnica");
+						break;
+				default:
+						alert("Afortunado en el amor");
+						break;
+
+			}
+		}
+		else
+		{
+			if(numeroUsuario > numeroSecreto)
+			{
+				alert("Usted se paso del numero secreto");
+
+			}
+			else
+			{
+				alert("A usted le faltan para llegar al numero secreto");
+
+			}
+		}
+		document.getElementById("intentos").value = contadorIntentos;
 }
--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -5,25 +5,120 @@ El jugador seleccionará una imagen correspondiente
 a su opción  y le informaremos si ganó, empató o perdió.
 */
 var eleccionMaquina;
+var eleccionUsuario;
+var piedra;
+var papel;
+var tijera;
+var piedra1;
+var papel1;
+var tijera1;
 
 function comenzar()
 {
-	
-
+    alert("Comenzamos!");
+    eleccionMaquina= Math.floor(Math.random()*3 +1);
+    console.log(eleccionMaquina);
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
+    if(eleccionMaquina==1)
+    {
+        eleccionMaquina=piedra;
+    }
+    else
+    {
+        if(eleccionMaquina == 2)
+        {
+            eleccionMaquina=papel;
+        }
+        else
+        {
+            eleccionMaquina=tijera;
+        }
+    }
 	
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
+     switch(eleccionMaquina)
+    {
+    case 1:
+        eleccionMaquina=piedra;
+        break;
+    case 2:
+        eleccionMaquina=papel;
+    case 3:
+        eleccionMaquina=tijera;
+    }
 
 
 }//FIN DE LA FUNCIÓN
+function piedra()
+{
+    piedra1=document.getElementById("piedra").value;
+    if(eleccionUsuario==piedra1 && eleccionMaquina==piedra)
+    {
+        alert("es un empate");
+    }
+    else
+    {
+        if(eleccionUsuario==piedra1 && eleccionMaquina==papel)
+        {
+            alert("perdiste");
+        }
+        else
+        {
+            alert("Ganaste!");
+        }
+    }
+}
 function tijera()
 {
+    
 	
 
 }//FIN DE LA FUNCIÓN
+
+function papel()
+{
+    papel1=document.getElementById("papel1").value;
+
+}//FIN DE LA FUNCION
+if(eleccionUsuario==papel1 && eleccionMaquina==papel)
+{
+    alert("es un empate");
+
+}
+else
+{
+    if(eleccionUsuario==papel1 && eleccionMaquina==tijera)
+    {
+        alert("perdiste");
+    }
+    else
+    {
+        alert("Ganaste!");
+    }
+
+}//FIN DE LA FUNCION
+function tijera()
+{
+    tijera1=document.getElementById("Tijera").value;
+    if(eleccionUsuario==tijera1 && eleccionMaquina==papel)
+    {
+        alert("Ganaste!");
+    }
+    else
+    {
+        if(eleccionUsuario==tijera1 && eleccionMaquina==piedra)
+        {
+            alert("perdiste");
+        }
+        else
+        {
+            alert("es un empate");
+        }
+    }
+}//FIN DE LA FUNCION
